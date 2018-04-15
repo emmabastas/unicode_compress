80f55f7e9a84460bfb0facf7519f31a1df63189c
Hugo Bastås <bastashugo@gmail.com>
2018-04-15 22:38:36 +0200

    fix bug in wich even amount of letter causes incorect encoding

--- unicode-compress.0.0.js
+++ unicode-compress.0.0.js
@@ -6,6 +6,8 @@ const unicodeCompress = {
 function compress(input) {
   let hexPairs = input.split("").map(x=>x.charCodeAt(0).toString(16))
   
+  if(hexPairs.length % 2 == 1) hexPairs.push("20")
+
   let hexQuads = []
   for(let i=0; i<hexPairs.length/2; i++) {
     hexQuads.push(hexPairs.slice(i*2, i*2+2).join(""))
